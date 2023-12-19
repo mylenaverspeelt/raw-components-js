d682369e7774665f614bdfbcf9bdc61b8632c518
mylenaverspeelt <mylena49@hotmail.com>
2023-12-19 10:07:55 -0300

    create build method component

---
+++ scripts/CardNews.js
@@ -0,0 +1,48 @@
+class CardNews extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({ mode: "open" })
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card_left");
+
+        const author = document.createElement("span");
+        author.textContent = "By " + (this.getAttribute("author") || "Anonimous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title") || "Sem conteúdo"
+        linkTitle.href = this.getAttribute("link-url")
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content") || "Sem conteúdo"
+
+        cardLeft.appendChild(author);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card_right");
+
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+        newsImage.src = this.getAttribute("img-src") || "assets/no-image.jpg"
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot; 
+    }
+
+    styles() {
+
+    }
+}
+
+customElements.define("card-news", CardNews)
